6e41c8ace040f8aeb1bd6e43c897a41ee1fb3239
Sajan Rajbhandari <sazanrjb@gmail.com>
2023-10-15 21:57:00 +0545

    Setup prisma

---
+++ migrations/20231015160846_create_setting_table/migration.sql
@@ -0,0 +1,18 @@
+-- CreateEnum
+CREATE TYPE "Type" AS ENUM ('enabled', 'disabled', 'within_working_hours');
+
+-- CreateTable
+CREATE TABLE "Setting" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "type" "Type" NOT NULL,
+    "workingHours" JSONB,
+    "senderId" UUID NOT NULL,
+    "message" TEXT,
+    "createdAt" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Setting_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Setting_senderId_key" ON "Setting"("senderId");
