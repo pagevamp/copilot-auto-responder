40d8700fea5ce6b93013a5351cf0444a7b216281
Sajan Rajbhandari <sazanrjb@gmail.com>
2023-10-30 18:06:46 +0545

    Secure webhook

--- src/app/api/messages/webhook/route.ts
+++ src/app/api/messages/webhook/route.ts
@@ -1,13 +1,25 @@
+import * as crypto from 'crypto';
 import { NextRequest, NextResponse } from 'next/server';
 import { MessageSchema } from '@/types/message';
 import { MessageService } from '@/app/api/messages/services/message.service';
+import appConfig from '@/config/app';
 
 export async function POST(request: NextRequest) {
-  const body = await request.json();
+  const rawBody = await request.text();
+
+  const body = JSON.parse(rawBody);
   const result = MessageSchema.safeParse(body);
 
   if (!result.success) {
     return NextResponse.json(result.error.issues, { status: 422 });
+  }
+
+  const signature = crypto.createHmac("sha256", appConfig.webhookSigningSecret).update(rawBody).digest("hex");
+
+  if (signature !== request.headers.get('x-copilot-signature')) {
+    return NextResponse.json({
+      message: 'Invalid signature'
+    }, { status: 400 })
   }
 
   const messageService = new MessageService();
