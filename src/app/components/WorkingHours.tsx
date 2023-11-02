62cb47b62b203fd01cb5d9841025bf2885551220
synsuman <synsuman@gmail.com>
2023-11-02 09:54:44 +0530

    UI changes to resize and fixes

--- src/app/components/WorkingHours.tsx
+++ src/app/components/WorkingHours.tsx
@@ -31,41 +31,43 @@ const WorkingHours = ({ selectedDays, errors }: Props) => {
 
       return (
         <li key={day}>
-          <div className="flex items-center">
+          <div className="flex flex-col gap-2 md:flex-row justify-between md:items-center">
             <Typography
               text={day.toLocaleLowerCase()}
               className="capitalize flex-1"
               variant="label"
             />
-            <div className="w-1/4 min-w-[100px] max-w-[200px]">
-              <Controller
-                name={`selectedDays[${selectedDayIndex}].startHour`}
-                render={({ field: { onChange, value } }) => (
-                  <SelectField<HOUR>
-                    value={value}
-                    options={HOURS_SELECT_OPTIONS}
-                    className="rounded-tr-none rounded-br-none border-r-0"
-                    onValueChange={(value: string) => {
-                      onChange(value);
-                    }}
-                  />
-                )}
-              />
-            </div>
-            <div className="w-1/4 min-w-[100px] max-w-[200px]">
-              <Controller
-                name={`selectedDays[${selectedDayIndex}].endHour`}
-                render={({ field: { onChange, value } }) => (
-                  <SelectField<HOUR>
-                    value={value}
-                    options={HOURS_SELECT_OPTIONS}
-                    className="rounded-tl-none rounded-bl-none"
-                    onValueChange={(value: string) => {
-                      onChange(value);
-                    }}
-                  />
-                )}
-              />
+            <div className="flex justify-end md:w-[372px]">
+              <div className="w-1/2 min-w-[100px] md:max-w-[200px]">
+                <Controller
+                  name={`selectedDays[${selectedDayIndex}].startHour`}
+                  render={({ field: { onChange, value } }) => (
+                    <SelectField<HOUR>
+                      value={value}
+                      options={HOURS_SELECT_OPTIONS}
+                      className="rounded-tr-none rounded-br-none border-r-0"
+                      onValueChange={(value: string) => {
+                        onChange(value);
+                      }}
+                    />
+                  )}
+                />
+              </div>
+              <div className="w-1/2 min-w-[100px] md:max-w-[200px]">
+                <Controller
+                  name={`selectedDays[${selectedDayIndex}].endHour`}
+                  render={({ field: { onChange, value } }) => (
+                    <SelectField<HOUR>
+                      value={value}
+                      options={HOURS_SELECT_OPTIONS}
+                      className="rounded-tl-none rounded-bl-none"
+                      onValueChange={(value: string) => {
+                        onChange(value);
+                      }}
+                    />
+                  )}
+                />
+              </div>
             </div>
           </div>
           {errors && (
